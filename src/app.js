5a1ffc73ca4489e3322127edb60f9d7c9d0fc5b7
戴程 <757214868@qq.com>
2021-07-28 18:01:31 +0800

    fix: commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router basename="/fastest-h5">
+      <Switch>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
