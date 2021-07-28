f805567d1cf6ef41dd91c903eedf62fd365672bd
戴程 <757214868@qq.com>
2021-07-28 18:02:20 +0800

    fix: commit

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,58 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+console.log(path.resolve(__dirname, '../src'))
+
 module.exports = {
-  entry: [path.resolve(__dirname, 'src')],
+  mode: "production",
+  entry: path.resolve(__dirname, '../src'),
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   module: {
     rules: [
       {
-        test: /\.(js|mjs|jsx|ts|tsx)$/,
-        include: [path.resolve(__dirname, 'src')],
-        exclude: [
-          path.resolve(__dirname, 'src/api'),
-          path.resolve(__dirname, 'src/utils')
-        ],
-        use: {
-          loader: 'babel-loader',
-        }
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: path.resolve(__dirname, '../src'),
+        use: 'babel-loader',
       },
-      {
-        test: /\.(css|less)$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: 'file-loader'
-      }
     ],
   },
   resolve: {
     //使用的拓展名
     extensions: [".js", ".json", ".jsx", ".css"],
     //使用的别名
-    alis: {
+    alias: {
       "@api": path.resolve(__dirname,'src/api'),
       "@components": path.resolve(__dirname,'src/components'),
       "@utils": path.resolve(__dirname,'src/utils'),
       "@config": path.resolve(__dirname,'src/config'),
       "@pages": path.resolve(__dirname,'src/pages'),
     },
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../public/index.html')
+    })
+]
 }
