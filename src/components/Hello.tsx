b36140eab7305768d2a3dee634e9eed3acb5d128
戴程 <757214868@qq.com>
2022-01-01 17:26:26 +0800

    fix: promise

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,32 +3,98 @@ import * as React from "react";
 interface HelloProps { compiler: string; framework: string; }
 
 const Hello = (props: HelloProps) => {
-    // 字符串的方法
-    // 1.split 方法的作用是使用指定的分隔符字符串将一个字符串对象分割成子字符串数组
-    // 参数：第一个参数是一个指定的分隔符字符串，也可以是正则表达式 第二个参数则是限定返回的数量
-    // 返回值： 返回的是一个子字符串的数组
-    const str = "hello world!";
-    const arr = str.split(" ");
-    const arr1 = str.split(" ", 1);
-    console.log(arr, arr1);
+    
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
 
-    // 2.slice 方法的作用是提取某个字符串的一部分，并返回一个新的字符串，且不会改变原字符串
-    // 参数： 第一个参数是从该索引处开始提取字符串中的字符，如果值为负数，会被当做是strLength + beginIndex看待
-    //        第二个参数是可选，在该索引出结束提取字符串。如果省略该参数则会一直提取到字符串末尾。
-    // 返回值： 返回的一个从原字符串中提取出来的心字符串
-    const str1 = 'The quick brown fox jumps over the lazy dog.';
-    const newStr1 = str1.slice(31);
-    console.log(newStr1);
+class newPromise {
+  state: any;
+  value: any;
+  reason: any;
+  successCallBack: any;
+  errorCallBack: any;
+  constructor(handler: Function) {
+    if (typeof handler !== 'function') {
+      throw `Promise构造函数的参数${handler}不是一个函数`
+    }
+    this.state = PENDING; //存储promise的状态，初始化状态为pending
+    this.value = undefined; //终值
+    this.reason =undefined; //拒因
+    this.successCallBack = [];
+    this.errorCallBack = [];
+    handler(this.resolve.bind(this), this.reject.bind(this));
+  }
+  resolve(value: any) {
+    if (this.state !== PENDING) return ;
+    this.state = FULFILLED;
+    this.value = value;
+    this.successCallBack.forEach((fn: any) => fn())
+  }
+  reject(reason: any) {
+    if (this.state !== PENDING) return ;
+    this.state = REJECTED;
+    this.reason = reason;
+    this.errorCallBack.forEach((fn: any) => fn())
+  }
+  then(onResolved: Function, onRejected?: Function) {
+    return new newPromise((resolve: any, reject: any) => {
+      onResolved = typeof onResolved === 'function' ? onResolved : (value: any) => value;
+      onRejected = typeof onRejected === 'function' ? onRejected : (value: any) => value;
+      if (this.state === FULFILLED) {
+        let result = onResolved(this.value);
+        this.handlerPromise(result, resolve, reject);
+      }
+      if (this.state === REJECTED) {
+        let result = onRejected(this.reason);
+        this.handlerPromise(result, resolve, reject);
+      }
+      if (this.state === PENDING) {
+        this.successCallBack.push(() => {
+          let result = onResolved(this.value);
+          this.handlerPromise(result, resolve, reject);
+        });
+        this.errorCallBack.push(() => {
+          let result = onRejected(this.reason);
+          this.handlerPromise(result, resolve, reject);
+        });
+      }
+    })
+  }
+  handlerPromise (result: any, resolve: any, reject: any) {
+    if(result instanceof Promise) {
+      result.then(resolve, reject);
+    } else {
+      resolve(result);
+    }
+  }
+}
 
-    //3.replace 方法的作用是替换部分或者所有匹配项的字符串
-    // 参数： 第一个参数正则或者字符串，表示需要被匹配替换的字符串（如果是字符串，仅替换第一个匹配项）
-    //        第二个参数字符串或者函数，用于替换掉第一个参数在原字符串中的匹配部分的字符串
-    // 返回值：一个部分或全部匹配由替换模式所取代的新的字符串。
-    const str2 = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
-    const newStr2 = str2.replace('dog', 'monkey');
-    const new1Str2 = str2.replace(/^dog$/, 'monkey')
-    console.log(newStr2, new1Str2);
 
+    const p = new newPromise((resolve: any, reject: any) => {
+        resolve("errr")
+    })
+    console.log(p)
+    p.then((value) => {
+      return new Promise((resolve, reject) => {
+        resolve("2222")
+      })
+    }).then((value) => {
+      console.log(value);
+    })
+
+
+    const s = new Promise((resolve, reject) => {
+      resolve("success")
+    })
+
+    s.then((value) => {
+      return new Promise((resolve, reject) => {
+        resolve("11")
+      })
+    }).then((value) => {
+      console.log(value)
+    })
     return (
         <div>
             <h1>Hello from {props.compiler} and {props.framework}!</h1>
--- src/utils/Es6-02.ts
+++ src/utils/Es6-02.ts
@@ -44,6 +44,16 @@
 // onRejected
 //    需要对onRejected类型进行验证，如果不是一个函数就设置为默认函数，这个函数时获取终值
 //    该函数在promise状态为rejected时候调用，传入拒因
+
+
+// then 方法需要支持多次调用
+// new Promise中的handler需要支持异步调用
+// then方法需要支持链式调用
+// then方法如果只传递一个参数，也需要返回正确的结果
+// 需要支持链式调用
+//    支持链式调用的条件
+//      只有then函数调用返回一个promise对象
+//      then函数有一个返回值
 const PENDING = 'pending';
 const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
@@ -52,8 +62,8 @@ class newPromise {
   state: any;
   value: any;
   reason: any;
-  successCallBack: null | Function;
-  errorCallBack: null | Function;
+  successCallBack: any;
+  errorCallBack: any;
   constructor(handler: Function) {
     if (typeof handler !== 'function') {
       throw `Promise构造函数的参数${handler}不是一个函数`
@@ -67,26 +77,70 @@ class newPromise {
     if (this.state !== PENDING) return ;
     this.state = FULFILLED;
     this.value = value;
-    this.successCallBack(this.value);
+    this.successCallBack.map((fn: any) => fn())
   }
   reject(reason: any) {
     if (this.state !== PENDING) return ;
     this.state = REJECTED;
     this.reason = reason;
-    this.errorCallBack(this.reason);
+    this.errorCallBack.map((fn: any) => fn())
   }
-  then(onResolved: Function, onRejected: Function) {
-    onResolved = typeof onResolved === 'function' ? onResolved : (value: any) => value;
-    onRejected = typeof onRejected === 'function' ? onRejected : (value: any) => value;
-    if (this.state === FULFILLED) {
-      onResolved(this.value);
-    }
-    if (this.state === REJECTED) {
-      onRejected(this.reason);
-    }
-    if (this.state === PENDING) {
-      this.successCallBack = onRejected;
-      this.errorCallBack = onRejected;
+  then(onResolved: Function, onRejected?: Function) {
+    return new newPromise((resolve: any, reject: any) => {
+      onResolved = typeof onResolved === 'function' ? onResolved : (value: any) => value;
+      onRejected = typeof onRejected === 'function' ? onRejected : (value: any) => value;
+      if (this.state === FULFILLED) {
+        let result = onResolved(this.value);
+        this.handlerPromise(result, resolve, reject);
+      }
+      if (this.state === REJECTED) {
+        let result = onRejected(this.reason);
+        this.handlerPromise(result, resolve, reject);
+      }
+      if (this.state === PENDING) {
+        this.successCallBack.push(() => {
+          let result = onResolved(this.value);
+          this.handlerPromise(result, resolve, reject);
+        });
+        this.errorCallBack.push(() => {
+          let result = onRejected(this.reason);
+          this.handlerPromise(result, resolve, reject);
+        });
+      }
+    })
+  }
+  handlerPromise (result: any, resolve: any, reject: any) {
+    if(result instanceof Promise) {
+      result.then(resolve, reject);
+    } else {
+      resolve(result);
     }
   }
 }
+
+
+// 测试
+const p = new newPromise((resolve: any, reject: any) => {
+  resolve("errr")
+})
+console.log(p)
+p.then(() => {
+return new Promise((resolve, reject) => {
+  resolve("2222")
+})
+}).then((value: any) => {
+  console.log(value); 
+})
+
+
+const s = new Promise((resolve, reject) => {
+resolve("success")
+})
+
+s.then((value) => {
+return new Promise((resolve, reject) => {
+  resolve("11")
+})
+}).then((value) => {
+  console.log(value)
+})
